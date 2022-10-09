10f7c27079a09bea26f30098b6ea20a99e16737b
Prafull <prafullpandey614@gmail.com>
2022-10-09 16:25:11 +0530

    Class based Views Convrted

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path ,include
 from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name = "starting-page"),
-    path("posts/", views.posts ,name="posts-page" ),
+    path("posts/", views.AllPostsView.as_view(),name="posts-page" ),
     # path("comment/handle", views.comment_handle),
-    path("path/<slug:slug>", views.post_detail ,name="post-detail-page"),
+    path("path/<slug:slug>", views.SinglePostView.as_view() ,name="post-detail-page"),
     # path("/posts/<slug:slug>", views.posts, name ="post-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView ,  DetailView
 from .models import Blog 
 # # Create your views here.
 # posts = {
@@ -31,15 +31,30 @@ class StartingPageView(ListView):
 #         "posts" : posts_collection,
 #     })
     # return HttpResponse("everythonng working")
-def posts(request):
-    posts_collection = Blog.objects.all().order_by("-date_d")
-    return render(request,'blog/posts.html',{
-        "posts":posts_collection,
-    })
-def post_detail(request,slug):
-    # identified_post = Blog.objects.get(slug=slug) I commented it because it might get error so we have another good django alternative
-    identified_post = get_object_or_404(Blog,slug=slug)
-    return render(request,'blog/post-detail.html',{
-        "post":identified_post,
-        "tags": identified_post.tags.all(),
-    })
+class AllPostsView(ListView):
+    template_name = "blog/posts.html"
+    model = Blog
+    ordering = ["-date_d"]
+    context_object_name = "posts"
+# def posts(request):
+#     posts_collection = Blog.objects.all().order_by("-date_d")
+#     return render(request,'blog/posts.html',{
+#         "posts":posts_collection,
+#     })
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Blog
+    context_object_name= "post"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = self.object.tags.all()
+        return context
+    
+    
+# def post_detail(request,slug):
+#     # identified_post = Blog.objects.get(slug=slug) I commented it because it might get error so we have another good django alternative
+#     identified_post = get_object_or_404(Blog,slug=slug)
+#     return render(request,'blog/post-detail.html',{
+#         "post":identified_post,
+#         "tags": identified_post.tags.all(),
+#     })
